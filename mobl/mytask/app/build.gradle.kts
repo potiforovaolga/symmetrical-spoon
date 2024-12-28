fdb065856c1aadf9ea8d4c47e69e617314060b47
potiforovaolga <potiforovaolga@gmail.com>
2024-12-29 01:36:56 +0300

    добавила рабочий вариант mytask

--- mobl/mytask/app/build.gradle.kts
+++ mobl/mytask/app/build.gradle.kts
@@ -1,4 +1,3 @@
-
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
@@ -6,12 +5,13 @@ plugins {
 }
 
 android {
-    namespace = "example.mytask"
-    compileSdk = 34
+    namespace = "com.example.mytask"
+    compileSdk = 35
 
     defaultConfig {
-        applicationId = "example.mytask"
+        applicationId = "com.example.mytask"
         minSdk = 26
+        //noinspection OldTargetApi
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
@@ -41,6 +41,7 @@ android {
 }
 
 dependencies {
+
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
@@ -49,6 +50,7 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+    implementation(libs.androidx.appcompat)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
@@ -56,6 +58,7 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.google.material)
+
 }
-
-
